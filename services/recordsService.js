2d1c3818f0a76934d758de0b6e8334cfbd0c1dc0
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-29 15:08:22 +0300

    add createNewRecord function

--- services/recordsService.js
+++ services/recordsService.js
@@ -1,16 +1,17 @@
 import recordsDal from "../dal/recordsDal.js";
+import { createError } from "../utils/utils.js";
 
 const createNewRecord = async (data, soldierId) => {
-    const isSoldierRecordExists = await recordsDal.getRecordBySoldierId(soldierId)
-    if (isSoldierRecordExists) {
-        const error = new Error(`soldier ${soldierId} already have benefits record`)
-        error.statusCode = 409
+    const soldierRecord = await recordsDal.getRecordBySoldierId(soldierId)
+    if (soldierRecord) {
+        const error = createError(409, `soldier ${soldierId} already have benefits record`)
+        throw error
     }
     const {unit, benefitType, details, decisionReason, budjetApproved, startDate} = data
     const newId = await recordsDal.writeNewRecord({soldierId, unit, currentBenefitType: null, history: []})
-    const isUpdated = await recordsDal.addBenefitToRecord({startDate, endDate: null, decisionReason, budjetApproved, benefitType, details})
-    if (!isUpdated) {
-        const err
-    }
-
+    await recordsDal.addBenefitToRecord({startDate, endDate: null, decisionReason, budjetApproved, benefitType, details})
+    const record = await recordsDal.getRecordById(newId)
+    return record
 }
+
+export default {createError}
