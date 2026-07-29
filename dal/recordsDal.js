85489961fc4fd2999ac3403e808b99ef4d0842ad
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-29 12:12:11 +0300

    add data access functions to create new record

---
+++ dal/recordsDal.js
@@ -0,0 +1,20 @@
+import db from "../dbConnections/mongodbConnection.js"
+import {ObjectId} from "mongodb"
+
+const collection = db.collection("records")
+
+const createNewRecord = async (record) => {
+    const result = await collection.insertOne(record);
+    return result.insertedId.toString()
+}
+
+const addBenefitToRecord = async (benefit, recordId) => {
+    const addToHistory = await collection.updateOne({_id: new ObjectId(recordId)}, { $push: { history: benefit} })
+    const changeCurrentType = await collection.updateOne({_id: new ObjectId(recordId)}, {$set: {currentBenefitType: benefit.benefitType}})
+    return addToHistory.modifiedCount
+}
+
+const getRecordById = async (recordId) => {
+    const record = await collection.findOne({_id: new ObjectId(recordId)})
+    return record
+}
