72594409174279cd029e6779ad6e9cc9b9d69c33
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-29 15:43:24 +0300

    add validation un create new record service

--- services/recordsService.js
+++ services/recordsService.js
@@ -8,10 +8,19 @@ const createNewRecord = async (data, soldierId) => {
         throw error
     }
     const {unit, benefitType, details, decisionReason, budjetApproved, startDate} = data
+    const benefitTypes = ["giftCart", "diningHall"]
+    if (!benefitTypes.includes(benefitType)) {
+        const error = createError(400, `${benefitType} is unauthorized benefit type`)
+        throw error
+    }
+    if (typeof budjetApproved !== "boolean") {
+        const error = createError(400, "budjetApproved field must be boolean")
+        throw error
+    }
     const newId = await recordsDal.writeNewRecord({soldierId, unit, currentBenefitType: null, history: []})
     await recordsDal.addBenefitToRecord({startDate, endDate: null, decisionReason, budjetApproved, benefitType, details})
     const record = await recordsDal.getRecordById(newId)
     return record
 }
 
-export default {createError}
+export default {createNewRecord}
