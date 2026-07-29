dc7fba30a7b8eb1b2bf5801c56936d24d7488e69
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-29 16:26:29 +0300

    connect recordRouter to app

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-import express from "express"
-import recordsRoter from "./routes/recordsRouter.js"
+import express from "express";
+import recordsRoter from "./routes/recordsRouter.js";
 
-const app = express()
+const app = express();
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
-    next()
-})
+    next();
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/soldiers", recordsRoter)
+app.use("/soldiers", recordsRoter);
 
 app.use((err, req, res, next) => {
-    const status = err.statusCode|| 500
-    const message = err.message || "server internal error"
-    res.status(status).send(message)
-})
+    console.log(err);
+    
+    const status = err.statusCode || 500;
+    const message = err.message || "server internal error";
+    res.status(status).send(message);
+});
 
-app.listen(process.env.PORT, () => console.log(`server is up and listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+    console.log(`server is up and listening on port ${process.env.PORT}`),
+);
+
