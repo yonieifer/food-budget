efd4cfd2d524d3ce39d1cf663e969fb331942eb9
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-30 10:44:48 +0300

    add budget router and saend to service req.body insted of distracted parts

--- routes/recordsRouter.js
+++ routes/recordsRouter.js
@@ -1,21 +1,19 @@
 import express from "express"
 import recordsService from "../services/recordsService.js"
 import { missingField } from "../utils/utils.js" 
-import { ReadConcern } from "mongodb"
 
 
 const router = express.Router()
 
 router.post ("/:soldiersId/benefits", async (req, res) => {
     const soldierId = req.params.soldiersId   
-    const {unit, benefitType, details, decisionReason, budjetApproved} = req.body
     const {startDate} = req.body || {startDate: new Date().toISOString()} 
-    const requiredFields = [unit, benefitType, details, decisionReason, budjetApproved]
+    const requiredFields = ["unit", "benefitType", "details", "decisionReason", "budjetApproved"]
     const missing = missingField(requiredFields, req.body)
     if (missing) {
             res.status(400).send(`field ${missing} is required`)
     }
-    const newRecord = await recordsService.createNewRecord({unit, benefitType, details, decisionReason, budjetApproved, startDate}, soldierId)
+    const newRecord = await recordsService.createNewRecord({...req.body, startDate}, soldierId)
     res.status(201).send(newRecord)
 })
 
@@ -34,7 +32,7 @@ router.patch("/:soldiersId/benefits", async (req, res) => {
     if (missing) {
             res.status(400).send(`field ${missing} is required`)
     }
-    const result = await recordsService.addNewBenefitToSoldier({benefitType, details, decisionReason, budjetApproved, decisionDate}, soldierId)
+    const result = await recordsService.addNewBenefitToSoldier({...req.body, decisionDate}, soldierId)
     if (result.reverted) {
         res.send({reverted: result.reverted, reason: "left foot", record: result.record})
     }
