6f12983f9fbd200023433e7ed9ee34f328183e99
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-30 16:06:11 +0300

    fix roter to router in app imports

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import recordsRoter from "./routes/recordsRouter.js";
-import budgetsRoter from "./routes/budgetRouter.js";
+import recordsRouter from "./routes/recordsRouter.js";
+import budgetsRouter from "./routes/budgetRouter.js";
 import { logger, errorHandler } from "./middleWares/middlewares.js";
 
 const app = express();
@@ -9,12 +9,12 @@ app.use(logger);
 
 app.use(express.json());
 
-app.use("/soldiers", recordsRoter);
+app.use("/soldiers", recordsRouter);
 
-app.use("/budget", budgetsRoter);
+app.use("/budget", budgetsRouter);
 
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
-    console.log(`server is up and listening on port ${process.env.PORT}`),
+    console.log(`server is up and listening on port ${process.env.PORT}...`),
 );
