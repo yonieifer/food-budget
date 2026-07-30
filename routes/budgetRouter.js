c8da98c639adabff9b4828a4ec3b0bc302d191d1
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-30 11:43:27 +0300

    add body check in post budget

--- routes/budgetRouter.js
+++ routes/budgetRouter.js
@@ -1,11 +1,12 @@
 import express from "express"
 import budgetService from "../services/budgetService.js"
 import { missingField } from "../utils/utils.js" 
+import { checkBody } from "../middleWares/middlewares.js"
 
 
 const router = express.Router()
 
-router.post("", (req, res) => {
+router.post("", checkBody, async (req, res) => {
     const requiredFields = ["unit", "benefitType", "month", "allocatedAmount"]
     const missing = missingField(requiredFields, req.body)
     if (missing) {
@@ -14,3 +15,5 @@ router.post("", (req, res) => {
     const newBudget = await budgetService.createNewBudget(req.body)
     res.status(201).send(newBudget)
 })
+
+export default router
