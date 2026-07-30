d3836f6a620d003b68d0754d38b0864797c7dd07
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-30 11:44:30 +0300

    add budget route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import recordsRoter from "./routes/recordsRouter.js";
+import budgetsRoter from "./routes/budgetRouter.js"
 import { logger, errorHandler } from "./middleWares/middlewares.js";
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(logger);
 app.use(express.json());
 
 app.use("/soldiers", recordsRoter);
+
+app.use("/budget", budgetsRoter)
 
 app.use(errorHandler);
 
