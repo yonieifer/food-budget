c12d44f82eeb5e848ec2fe01115368fbc49c0dc5
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-30 11:24:02 +0300

    add middlewares: logger, check body, error handling

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 import express from "express";
 import recordsRoter from "./routes/recordsRouter.js";
+import { logger, errorHandler } from "./middleWares/middlewares.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log(req.method, req.url);
-    next();
-});
+app.use(logger);
 
 app.use(express.json());
 
 app.use("/soldiers", recordsRoter);
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    
-    const status = err.statusCode || 500;
-    const message = err.message || "server internal error";
-    res.status(status).send(message);
-});
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
     console.log(`server is up and listening on port ${process.env.PORT}`),
---
+++ middleWares/middlewares.js
@@ -0,0 +1,21 @@
+import { createError } from "../utils/utils.js"
+
+export const checkBody = (req, res, next) => {
+    if(!req.body) {
+        const error = createError(400, "no body")
+        throw error
+    }
+    next()
+}
+
+export const logger = (req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+}
+
+export const errorHandler = (err, req, res, next) => {
+    console.log(err);
+    const status = err.statusCode || 500;
+    const message = err.message || "server internal error";
+    res.status(status).send(message);
+}
